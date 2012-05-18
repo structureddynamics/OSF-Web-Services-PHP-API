bc95883a8a191c2f428e2aa2cf5f4fe8ec5124cf
Frederick Giasson <fred@fgiasson.com>
2012-05-18 14:54:32 -0400

    Add a new utility class to specify what are the CRUD permission related to different things (dataset access, access records, etc).

---
+++ StructuredDynamics/structwsf/php/api/framework/CRUDPermission.php
@@ -0,0 +1,68 @@
+<?php
+
+/*! @ingroup StructWSFPHPAPIFramework Framework of the structWSF PHP API library */
+//@{
+
+/*! @file \StructuredDynamics\structwsf\php\api\framework\CRUDPermission.php
+
+   An internal CRUDPermission class
+  
+   @author Frederick Giasson, Structured Dynamics LLC.
+*/
+
+
+namespace StructuredDynamics\structwsf\php\api\framework;
+
+/** 
+* The CRUD Permission class is used to define CRUD permissions
+* used by some of the web service classes wrappers.
+* 
+* @author Frederick Giasson, Structured Dynamics LLC.
+*/
+
+class CRUDPermission
+{  
+  private $create = FALSE;
+  private $read = FALSE;
+  private $update = FALSE;
+  private $delete = FALSE;
+  
+  /**
+  * Constructor
+  * 
+  * @param mixed $create Boolean that specify if Create operations can be performed.
+  * @param mixed $read Boolean that specify if Read operations can be performed.
+  * @param mixed $update Boolean that specify if Update operations can be performed.
+  * @param mixed $delete Boolean that specify if Delete operations can be performed.
+  * @return CRUDPermission
+  */
+  function __construct($create, $read, $update, $delete)
+  {
+    $this->create = $create;
+    $this->read = $read;
+    $this->update = $update;
+    $this->delete = $delete;
+  }
+  
+  function getCreate()
+  {
+    return($this->create);
+  }
+  
+  function getRead()
+  {
+    return($this->read);
+  }
+     
+  function getUpdate()
+  {
+    return($this->update);
+  }
+  
+  function getDelete()
+  {
+    return($this->delete);
+  }
+}
+  
+?>
