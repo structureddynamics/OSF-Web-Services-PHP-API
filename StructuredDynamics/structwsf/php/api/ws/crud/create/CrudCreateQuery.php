6512bca7fc534b87a9c418e8ba6270949e471e3c
Frederick Giasson <fred@fgiasson.com>
2012-05-22 11:23:56 -0400

    Added some more usage notes for the CRUD: Create endpoint.

--- StructuredDynamics/structwsf/php/api/ws/crud/create/CrudCreateQuery.php
+++ StructuredDynamics/structwsf/php/api/ws/crud/create/CrudCreateQuery.php
@@ -20,6 +20,29 @@
   * When a new instance record is created, it becomes accessible to the 
   * users that have access to this dataset. 
   * 
+  * Usage notes: This Web service is intended to be used by any user that wants to update 
+  * the description of an instance record. The update of a record is performed by two atomic 
+  * actions: delete and create. All records described within the input RDF document will get 
+  * updated on the system. For each of them, the Crud Update web service will remove all the 
+  * triples we have defined for them in the target dataset, and then will re-insert the new 
+  * ones.
+  * 
+  * This is the main difference between an Update of a given instance record, and the Creation 
+  * (using Crud Create) of an already existing record: the update web service guaranties that 
+  * only the triples of the updated version of an instance record will be in the system. 
+  * Creating an already existing instance record, will overwrite existing triples, and will 
+  * add new ones. But the ones from the old version of the instance record that are not in 
+  * the new version won't be delete in the triple store instance, but will be deleted in 
+  * the Solr instance because Solr documents can't be updated (they can only be replaced).
+  * 
+  * It also update possible reification statements.
+  * 
+  * Warning: if your RDF document contains blank nodes and that you try to update them using 
+  * the CRUD: Update web service endpoint, this will results in the creation of a new set of 
+  * resources with new blank nodes URIS. This means that resources specified as blank nodes 
+  * can't be updated using this web service endpoint. The best practice is not using blank 
+  * nodes.
+  * 
   * @see http://techwiki.openstructs.org/index.php/CRUD:_Create
   * 
   * @author Frederick Giasson, Structured Dynamics LLC.  
